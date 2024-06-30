3df27de24316ddfc97706bd71c41db5f9a02cf6d
NrkBeats <simon.nnoruka@viacesi.fr>
2024-06-30 16:51:19 +0200

    model fini

--- backend/app/models/wedding.ts
+++ backend/app/models/wedding.ts
@@ -4,6 +4,8 @@ import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 
 import User from '#models/user'
 import Gallery from '#models/gallery'
+import Program from './program.js'
+import Donation from './donation.js'
 export default class Wedding extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
@@ -29,11 +31,23 @@ export default class Wedding extends BaseModel {
   @column()
   declare userId: number
 
+  @column()
+  declare programId: number
+
+  @column()
+  declare donationId: number
+
   @belongsTo(() => Gallery)
   declare gallery: BelongsTo<typeof Gallery>
 
   @belongsTo(() => User)
   declare user: BelongsTo<typeof User>
+
+  @belongsTo(() => Program)
+  declare program: BelongsTo<typeof Program>
+
+  @belongsTo(() => Donation)
+  declare donation: BelongsTo<typeof Donation>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
